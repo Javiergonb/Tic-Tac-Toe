9c27d29e4db356ed2eac81e25f113113d4ed5b1d
Javiergonb <javiergonb3@gmail.com>
2025-01-08 18:17:14 -0300

    feat: Can now play full game with winning and losing and drawing
    Implemented a matrix traversal algo for seeing if someone has won or not

--- logic.js
+++ logic.js
@@ -5,7 +5,7 @@
 function GameBoard(){
     const board = []; //private?
 
-    //Populate the board with nulls
+    //Populate the board with 0s
     for(let row = 0; row<3;row++){
         board[row] = [];
         for(let col = 0; col<3;col++){
@@ -36,7 +36,7 @@ function GameBoard(){
         }
 
         board[row][col].addSymbol(playerToken);
-        console.log(`Marked with ${playerToken}`)
+        //console.log(`Marked with ${playerToken}`)
 
     }    
     
@@ -45,7 +45,66 @@ function GameBoard(){
         console.log(boardWithCellValues);
     };
 
-    return {getBoard,printBoard,markCell};
+    const checkBoardHasWinner = (token) =>{
+        //Check for rows
+        for(let row = 0; row<3; row++){
+            let countOfTokens = 0;
+            for(let col= 0; col < 3; col++){
+                if(board[row][col].getValue() === token){
+                    countOfTokens++;
+                }
+            }
+            if(countOfTokens==3){
+                console.log(countOfTokens);
+                return true;
+            } 
+        }
+
+        //Check for columns
+        for(let row = 0; row<3; row++){
+            let countOfTokens = 0;
+            for(let col= 0; col < 3; col++){
+                if(board[col][row].getValue() === token){
+                    countOfTokens++
+                }
+            }
+            if(countOfTokens==3){
+                return true;
+            }
+        }
+
+        //check for diagonal
+        let countOfTokens = 0;
+        for(let i= 0; i<3;i++){
+            if(board[i][i].getValue() === token){
+                countOfTokens++
+            }
+            if(countOfTokens==3){
+                return true;
+            }
+        }
+
+        countOfTokens = 0;
+        for(let i=0;i<3;i++){
+            if(board[2-i][i].getValue() === token){
+                countOfTokens++
+            }
+            if(countOfTokens==3){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    const resetBoard = () =>{
+        for(let row = 0; row<3;row++){
+            for(let col = 0; col<3;col++){
+                board[row][col].addSymbol(0); //Again we can call Cell() to crea te new cell object without new!
+            }
+        }
+    }
+
+    return {getBoard,printBoard,markCell,checkBoardHasWinner,resetBoard};
 }
 
 //We need a cell object so that we can write to it
@@ -76,6 +135,7 @@ function Player(name,token){
 
 function gameController(){
     const board = (function(){return GameBoard()})();
+    let round = 0;
     const players = [
         (function(){return Player("Javier","X")})(),
         (function(){return Player("Isidora","O")})()
@@ -96,14 +156,47 @@ function gameController(){
     }
 
     const playRound = (row,col) =>{
-        console.log(`${getActivePlayer().name} marking his chosen cell`)
+        //console.log(`${getActivePlayer().name} marking his chosen cell`)
         board.markCell(row,col,getActivePlayer().token);
 
         //We should also check if someone has won. If they have we end the game if not we play a new round and game continues
+        const hasWinner = board.checkBoardHasWinner(getActivePlayer().token)
+        if(hasWinner){
+            gameFinish();
+            return;
+        }
+
+        
+
 
         switchPlayerTurn();
+        round++;
+        if(!hasWinner && round == 9){
+            gameDraw();
+            return;
+        }
+        printNewRound();
+        
+    }
+
+    const gameFinish = ()=> {
+        console.log(`${getActivePlayer().name} HAS WON!`)
+        startNewGame();
+    };
+
+    const startNewGame = ()=>{
+        console.log("STARTING NEW GAME")
+        board.resetBoard();
+        round = 0;
+        activePlayer = players[0]
         printNewRound()
     }
+
+    const gameDraw = () =>{
+        console.log("ITS A DRAW!")
+        startNewGame();
+    }
+
 
     // Initial play game message
     printNewRound();
@@ -114,10 +207,74 @@ function gameController(){
       };
 }
 
-const controller = gameController();
+const controller = (function() {return gameController()})();
 
-controller.playRound(0,0);
-controller.playRound(1,2);
+//row WIN
+controller.playRound(0, 0)  // X plays
+controller.playRound(1, 1)  // O plays
+controller.playRound(0, 1)  // X plays
+controller.playRound(2, 2)  // O plays
+controller.playRound(0, 2)  // X wins
+
+controller.playRound(1, 0)   
+controller.playRound(0, 0)   
+controller.playRound(1, 1)   
+controller.playRound(2, 2)   
+controller.playRound(1, 2)   
+
+controller.playRound(2, 0)  
+controller.playRound(1, 1)  
+controller.playRound(2, 1)  
+controller.playRound(0, 0)  
+controller.playRound(2, 2)  
+
+
+//Column WIN
+controller.playRound(0, 0)  
+controller.playRound(0, 1)  
+controller.playRound(1, 0)  
+controller.playRound(2, 2)  
+controller.playRound(2, 0) 
+
+controller.playRound(0, 1)  
+controller.playRound(0, 0)  
+controller.playRound(1, 1)  
+controller.playRound(2, 0)  
+controller.playRound(2, 1) 
+
+controller.playRound(0, 2)  
+controller.playRound(1, 1)  
+controller.playRound(1, 2)  
+controller.playRound(0, 0)  
+controller.playRound(2, 2) 
+
+
+//diagonal
+controller.playRound(0, 0)  
+controller.playRound(0, 1)  
+controller.playRound(1, 1)  
+controller.playRound(2, 1)  
+controller.playRound(2, 2) 
+
+//antidiagonal
+controller.playRound(0, 2)  
+controller.playRound(0, 0)  
+controller.playRound(1, 1)  
+controller.playRound(2, 2)  
+controller.playRound(2, 0)  
+
+//DRAW
+
+controller.playRound(0, 0)  
+controller.playRound(0, 1)  
+controller.playRound(0, 2)  
+controller.playRound(1, 1)  
+controller.playRound(1, 0)  
+controller.playRound(1, 2)  
+controller.playRound(2, 1)  
+controller.playRound(2, 0)  
+controller.playRound(2, 2)  
+
 
 
 
